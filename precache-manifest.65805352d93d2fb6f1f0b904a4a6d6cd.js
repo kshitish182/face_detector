886940bfe6afb8476d8915894189246f34b638b1
kshitish182 <kshitish182@gmail.com>
2018-12-28 11:06:36 +0545

    Updates

---
+++ precache-manifest.65805352d93d2fb6f1f0b904a4a6d6cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed56d0ef5ed158aaf5d0",
+    "url": "/face_detector/static/js/runtime~main.ed56d0ef.js"
+  },
+  {
+    "revision": "491bb2b9f7752a5341b0",
+    "url": "/face_detector/static/js/main.491bb2b9.chunk.js"
+  },
+  {
+    "revision": "2bd3564d9aa289470271",
+    "url": "/face_detector/static/js/1.2bd3564d.chunk.js"
+  },
+  {
+    "revision": "491bb2b9f7752a5341b0",
+    "url": "/face_detector/static/css/main.a9be00e7.chunk.css"
+  },
+  {
+    "revision": "83439e715bc6de45b160f95320110e28",
+    "url": "/face_detector/index.html"
+  }
+];
